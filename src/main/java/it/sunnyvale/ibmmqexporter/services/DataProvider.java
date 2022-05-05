fef82641a61b47796bfd5820728b60695fb364f4
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2022-05-05 16:12:58 +0200

    QueueManager reuse

--- src/main/java/it/sunnyvale/ibmmqexporter/services/DataProvider.java
+++ src/main/java/it/sunnyvale/ibmmqexporter/services/DataProvider.java
@@ -4,33 +4,97 @@ import com.ibm.mq.MQC;
 import com.ibm.mq.MQEnvironment;
 import com.ibm.mq.MQQueue;
 import com.ibm.mq.MQQueueManager;
+import it.sunnyvale.ibmmqexporter.models.QueueManager;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
 
 @Service
 @Scope("singleton")
 public class DataProvider {
 
+    private HashMap<String,MQQueueManager> queueManagerHashMap;
+
+    @PostConstruct
+    public void init(){
+        queueManagerHashMap = new HashMap<>();
+    }
+
+
+    private MQQueueManager createQueueManager(String host,
+                                              String port,
+                                              String queueManagerName,
+                                              String channel,
+                                              String username,
+                                              String password
+    ) {
+        try{
+            MQEnvironment.hostname = host;
+            MQEnvironment.port = Integer.valueOf(port).intValue();
+            MQEnvironment.channel = channel;
+            MQEnvironment.properties.put(MQC.TRANSPORT_PROPERTY,
+                    MQC.TRANSPORT_MQSERIES);
+            MQQueueManager queueManager = new MQQueueManager(queueManagerName);
+            queueManagerHashMap.put(queueManagerName,queueManager);
+            return queueManager;
+        } catch (Exception err) {
+            throw new RuntimeException(err);
+        }
+    }
+
+    private MQQueueManager getOrCreateQueueManager(String host,
+                                                   String port,
+                                                   String queueManagerName,
+                                                   String channel,
+                                                   String username,
+                                                   String password
+                            ){
+        if (queueManagerHashMap.containsKey(queueManagerName)){
+            MQQueueManager queueManager = queueManagerHashMap.get(queueManagerName);
+            if( !queueManager.isOpen() || !queueManager.isConnected()){
+                queueManager = createQueueManager(host,
+                        port,
+                        queueManagerName,
+                        channel,
+                        username,
+                        password
+                );
+                queueManagerHashMap.put(queueManagerName,queueManager);
+            }
+            return queueManager;
+        }else{
+            return createQueueManager(host,
+                    port,
+                    queueManagerName,
+                    channel,
+                    username,
+                    password
+                    );
+        }
+    }
+
     public double getQueueCurrentDepth(String host,
                                        String port,
-                                       String queueManager,
+                                       String queueManagerName,
                                        String channel,
                                        String username,
                                        String password,
                                        String queue){
         int depth = -1;
         try {
-            MQEnvironment.hostname = host;
-            MQEnvironment.port = Integer.valueOf(port).intValue();
-            MQEnvironment.channel = channel;
-            MQEnvironment.properties.put(MQC.TRANSPORT_PROPERTY,
-                    MQC.TRANSPORT_MQSERIES);
-            MQQueueManager qMgr = new MQQueueManager(queueManager);
+            MQQueueManager queueManager = getOrCreateQueueManager(host,
+                    port,
+                    queueManagerName,
+                    channel,
+                    username,
+                    password
+                    );
             int openOptions = MQC.MQOO_INQUIRE;
-            MQQueue destQueue = qMgr.accessQueue(queue, openOptions);
+            MQQueue destQueue = queueManager.accessQueue(queue, openOptions);
             depth = destQueue.getCurrentDepth();
             destQueue.close();
-            qMgr.disconnect();
         } catch (Exception err) {
             throw new RuntimeException();
         }
